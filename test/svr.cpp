f83f9c824bb7dd03c3772202151e29ac1fa1819d
liuyongshuai <liuyongshuai@didichuxing.com>
2019-04-25 10:49:10 +0800

    modify files

---
+++ test/svr.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+ * File:	svr.cpp
+ * Author:	liuyongshuai<liuyongshuai@hotmail.com>
+ * Time:	2019-04-25 10:44
+ ************************************************************************/
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<stdio.h>
+#include<stdint.h>
+
+int main(){
+    return 0;
+}
