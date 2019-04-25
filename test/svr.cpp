76359369c5bfa52c4b18f34bc3e007b8f0d3b255
liuyongshuai <liuyongshuai@didichuxing.com>
2019-04-25 16:24:06 +0800

    modify files

--- test/svr.cpp
+++ test/svr.cpp
@@ -8,6 +8,31 @@
 #include<string.h>
 #include<stdio.h>
 #include<stdint.h>
+#include<sys/epoll.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<fcntl.h>
+#include<deque>
+#include<map>
+#include<vector>
+#include<pthread.h>
+#include<semaphore.h>
+#include<sys/time.h>
+#include<sys/shm.h>
+#include<errno.h>
+#include<string>
+#include<signal.h>
+#include<sys/stat.h>
+#include<cctype>
+#include<sstream>
+#include<utility>
+#include<stdexcept>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<iostream>
+
+
+using namespace std;
 
 int main(){
     return 0;
