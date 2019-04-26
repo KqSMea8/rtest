3d577a06b4935e6578450846e062d53d77d9142a
liuyongshuai@hotmail.com <liuyongshuai@hotmail.com>
2019-04-26 23:04:54 +0800

    modify files

--- src/httpServer/httpRouter.cpp
+++ src/httpServer/httpRouter.cpp
@@ -5,9 +5,64 @@
  ************************************************************************/
 #include<iostream>
 #include "httpRouter.h"
+#include "../utils/strutils.h"
 
 using namespace std;
 
 namespace runtofuServer{
+    //添加一条路由信息
+    void httpRouter::addRouter(const routerItem *r){
+        this->addRouter(r->type, r->config, r->func, r->extParam);
+    }
 
+    void httpRouter::addRouter(const routerItem &r){
+        this->addRouter(&r);
+    }
+
+    void httpRouter::addRouter(ROUTER_TYPE t, const string &c, httpFunc f, const string &extParam){
+        routerItem *ritem = new routerItem;
+        ritem->type = t;
+        ritem->config = c;
+        ritem->func = f;
+        ritem->extParam = extParam;
+        routerList.push_back(ritem);
+    }
+
+    //匹配路由规则
+    const routerItem *httpRouter::matchRouter(const string &uri, map <string, string> &args){
+        const routerItem *ret = NULL;
+        string tmpUri = uri;
+        StrUtils::trimChar(tmpUri, '/');
+        vector< routerItem * >::const_iterator iter;
+        for (iter = routerList.begin(); iter != routerList.end(); iter++){
+            if ((*iter)->type == ROUTER_TYPE_PATH_INFO){
+                ret = matchPathInfoRouter(uri, *iter, args);
+                if (ret != NULL){
+                    return ret;
+                }
+            }
+            else if ((*iter)->type == ROUTER_TYPE_REGEXP){
+                ret = matchRegexpRouter(uri, *iter, args);
+                if (ret != NULL){
+                    return ret;
+                }
+            }
+        }
+        return ret;
+    }
+
+    const routerItem *httpRouter::matchPathInfoRouter(const string &uri, const routerItem *router, map <string, string> &args){
+        return NULL;
+    }
+
+    const routerItem *httpRouter::matchRegexpRouter(const string &uri, const routerItem *router, map <string, string> &args){
+        return NULL;
+    }
+
+    httpRouter::~httpRouter(){
+        vector< routerItem * >::iterator iter;
+        for (iter = routerList.begin(); iter != routerList.end(); iter++){
+            delete *iter;
+        }
+    }
 } //namespace runtofuServer
--- src/httpServer/httpRouter.h
+++ src/httpServer/httpRouter.h
@@ -17,27 +17,59 @@ namespace runtofuServer{
     //要执行的方法
     typedef void (*httpFunc)(httpRequest &req, httpResponse &rsp);
 
+    //路由匹配类型
+    typedef enum _router_type{
+        ROUTER_TYPE_PATH_INFO = 1,//全路径匹配
+        ROUTER_TYPE_REGEXP//正则匹配
+    } ROUTER_TYPE;
+
+    //单个路由匹配项
+    typedef struct _router_item{
+        ROUTER_TYPE type;//路由匹配类型
+        std::string config;//规则信息
+        httpFunc func;//控制层
+        std::string extParam;//额外的参数
+    } routerItem;
+
     class httpRouter{
     public:
-        //路由匹配类型
-        typedef enum _router_type{
-            ROUTER_TYPE_PATH_INFO = 1,//全路径匹配
-            ROUTER_TYPE_REGEXP//正则匹配
-        } ROUTER_TYPE;
-
-        //单个路由匹配项
-        typedef struct _router_item{
-            ROUTER_TYPE type;//路由匹配类型
-            std::string config;//规则信息
-            httpFunc func;//控制层
-            std::string extParam;//额外的参数
-        } routerItem;
+        ~httpRouter();
 
         //路由列表信息
         std::vector< routerItem * > routerList;
 
         //添加一条路由信息
-        void addRouter();
+        void addRouter(const routerItem *r);
+
+        void addRouter(const routerItem &r);
+
+        void addRouter(ROUTER_TYPE t, const std::string &c, httpFunc f, const std::string &extParam);
+
+        //匹配路由规则，如果有参数的话一起返回
+        const routerItem *matchRouter(const std::string &uri, std::map <std::string, std::string> &args);
+
+    private:
+        /**
+         * 匹配全路径路由
+         *      :arg	只配置数字类型
+         *      :arg:	可以匹配任意类型
+         * 如：
+         *      /ggtest/:id	匹配下面的URL，并把 id作为参数回传给controller层
+         *          /ggtest/6666
+         *          /ggtest/89999
+         *      /ggtest/:name:	匹配下面的URL。并把name作为参数回传给controller层
+         *          /ggtest/wendao
+         *          /ggtest/abc
+         *          /ggtest/44444
+         **/
+        const routerItem *matchPathInfoRouter(const std::string &uri, const routerItem *router, std::map <std::string, std::string> &args);
+
+        /**
+         * 匹配正则路由，直接用正则表达式去匹配请求的URL，并把捕获的参数回传到Param配置里,如
+         * config 为 `^ggtest/aid(\w+?)/cid(\d+)$`，Param 为 aid=$1&cid=$2
+         * 则将请求中aid后面的字符串挑出来赋给aid，cid后面的字符串挑出来赋给cid
+         */
+        const routerItem *matchRegexpRouter(const std::string &uri, const routerItem *router, std::map <std::string, std::string> &args);
     };
 } //namespace runtofuServer
 
